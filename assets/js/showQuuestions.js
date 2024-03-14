842f8d6748dd7992a771db59c431532e75041d00
arunp44 <137665388+arunp44@users.noreply.github.com>
2024-03-14 12:45:01 -0600

    final push

---
+++ assets/js/showQuuestions.js
@@ -0,0 +1,25 @@
+var myQuestions = [
+	{
+		question: "What is the capital of Australia?",
+		answers: {
+			options: ["Sydney", "Melbourne", "Canberra", "Perth"],
+		},
+		correctAnswer: "Canberra"
+	},
+	{
+		question: "What is 30/3?",
+		answers: {
+			a: '3',
+			b: '5',
+			c: '10'
+		},
+		correctAnswer: 'c'
+	}
+];
+
+
+var question1 = {
+    question: "What is the capital of Australia?",
+    options: ["Sydney", "Melbourne", "Canberra", "Perth"],
+    answer: "Canberra"
+};
